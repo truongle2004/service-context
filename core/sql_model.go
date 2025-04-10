d0e0112ed209bf9f776a09954dfc074d26eb0ea2
truongle2004 <119171229+truongle2004@users.noreply.github.com>
2025-04-11 06:47:27 +0700

    fix

--- core/sql_model.go
+++ core/sql_model.go
@@ -4,16 +4,17 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type SQLModel struct {
-	ID        string    `json:"-" gorm:"primaryKey;autoIncrement"`
+	ID        string    `json:"-" gorm:"primaryKey;"`
 	CreatedAt time.Time `json:"created_at,omitempty" gorm:"autoCreateTime;column:created_at"`
 	UpdatedAt time.Time `json:"updated_at,omitempty" gorm:"autoUpdateTime;column:updated_at"`
 }
 
 // BeforeCreate - Automatically assigns a UUID to FakeId before saving
-func (sqlModel *SQLModel) BeforeCreate(objectId int) {
+func (sqlModel *SQLModel) BeforeCreate(ctx *gorm.DB) {
 	// Generate uuid and store it as FakeId
 	sqlModel.ID = uuid.New().String()
 }
