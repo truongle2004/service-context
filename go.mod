2623c6e557eb9c428fff8f345b1afbdd2b81aa6d
truongle2004 <119171229+truongle2004@users.noreply.github.com>
2025-04-11 17:18:31 +0700

    fix

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/google/uuid v1.6.0
 require github.com/pkg/errors v0.9.1
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/text v0.24.0 // indirect
---
+++ token/validate.go
@@ -0,0 +1,29 @@
+package token
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func validateToken(tokenString string, publicKey *ecdsa.PublicKey) (*jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Ensure token uses ES256
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return publicKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate token and return claims
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return &claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token")
+}
