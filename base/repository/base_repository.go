45efe9a5eab9704e3e1904bc43771d056b815f4d
truongle2004 <119171229+truongle2004@users.noreply.github.com>
2025-05-08 09:33:13 +0700

    update

--- base/repository/base_repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type BaseRepository[T any] interface {
-	Create(entity *T) error
-	GetByID(id string) (*T, error)
-	Update(entity *T) error
-	Delete(id string) error
-	List() ([]*T, error)
-}
