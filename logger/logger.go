2a2afc539e42316aec1b26c6ed16f902b29a4153
truongle2004 <119171229+truongle2004@users.noreply.github.com>
2025-05-18 14:36:03 +0700

    update

---
+++ logger/logger.go
@@ -0,0 +1,35 @@
+package logger
+
+import "github.com/charmbracelet/log"
+
+func Info(msg string) {
+	log.Info(msg)
+}
+
+func Infof(format string, v ...any) {
+	log.Infof(format, v...)
+}
+
+func Error(msg string) {
+	log.Error(msg)
+}
+
+func Errorf(format string, v ...any) {
+	log.Errorf(format, v...)
+}
+
+func Debug(msg string) {
+	log.Debug(msg)
+}
+
+func Debugf(format string, v ...any) {
+	log.Debugf(format, v...)
+}
+
+func Warn(msg string) {
+	log.Warn(msg)
+}
+
+func Warnf(format string, v ...any) {
+	log.Warnf(format, v...)
+}
